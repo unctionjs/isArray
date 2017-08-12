232b2d72bbfe167137b563686ff34d087a616852
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:11:18 -0700

    Woops, totally fucked up these paths

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, no-magic-numbers, no-undefined */
 import {test} from "tap"
 
-import ifThenElse from "./source"
+import isArray from "./source"
 
 test(({notOk, end}) => {
   notOk(isArray(null))
